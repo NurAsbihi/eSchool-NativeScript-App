9fa31664c2339f761214d5f1dd330b7b4ac2622f
ipxez <sleequil@gmail.com>
2021-03-18 20:47:25 +0000

    Finished - May need styling

--- app/components/LessonsList.vue
+++ app/components/LessonsList.vue
@@ -1,9 +1,19 @@
 <template>
-    <ListView for='lesson in lessons'>
-        <v-template>
-            <Label :text='lesson.name' />
-        </v-template>
-    </ListView>
+    <StackLayout>
+        <Label class="h2 text-center" text="Lessons" />
+        <Label class="h2 text-center" text="Tap a product to add it to the cart" />
+        <ListView for='lesson in lessons' @itemTap='onItemTap'>
+            <v-template>
+                <StackLayout>
+                    <Label :text='lesson.image' />
+                    <Label :text='lesson.name' />
+                    <Label :text='lesson.location' />
+                    <Label :text="`£${lesson.price}`" />
+                    <Label :text="`Available: ${lesson.spaces}`" />
+                </StackLayout>
+            </v-template>
+        </ListView>
+    </StackLayout>
 </template>
 
 
@@ -11,12 +21,28 @@
     export default  {
         data() {
             return  {
-                lessons: [ 
-                    {name: "Hi"},
-                    {name: "Hi2"},
-                    {name: "Hi3"}
-                ]
+                lessons: {}
             };
+        },
+        methods:    {
+            onItemTap(event)    {
+                this.$emit('addLesson', event.item);
+                alert(event.item.name)
+            },
+            
+        },
+        created: function() {
+            fetch('https://cw2-server.herokuapp.com/collection/products', {
+                method: 'GET',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify()
+            })
+            .then(response => response.json())
+            .then((data) =>    {
+                this.lessons = data;
+            })
         },
     }
 </script>
