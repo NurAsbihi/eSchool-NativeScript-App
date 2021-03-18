de187dd632c815d839d60664b36512125789ad50
ipxez <sleequil@gmail.com>
2021-03-18 20:46:54 +0000

    Added Props & Ability to remove items from cart

--- app/components/Checkout.vue
+++ app/components/Checkout.vue
@@ -1,11 +1,31 @@
 <template>
-    
+    <StackLayout>
+        <Label class="h2 text-center" text="Shopping Cart" />
+        <Label class="h2 text-center" text="Tap a product to remove it from the cart" />
+        <ListView for="lesson in cart" @itemTap='onItemTap'>
+            <v-template>
+                <StackLayout>
+                    <Label :text="lesson.name" />
+                    <Label :text="`£${lesson.price}`" />
+                </StackLayout>
+            </v-template>
+        </ListView>
+    </StackLayout>
 </template>
 
 <script>
-    export default defineComponent({
+    export default {
+        props: ['cart'],
         data() {
-            
+            return  {
+                
+            }
         },
-    })
+        methods:    {
+            onItemTap(event)    {
+                this.$emit('removeItem', event.item);
+                alert("Removed " + event.item.name + " from cart")
+            }
+        }       
+    }
 </script>
